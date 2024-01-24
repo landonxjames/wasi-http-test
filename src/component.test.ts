c5f3f9c1ba1a5fb0381841a8e9b21e87d12c247a
Landon James <lnj@amazon.com>
2024-01-23 17:12:24 -0800

    cleaning up test file

--- src/component.test.ts
+++ src/component.test.ts
@@ -1,62 +1,27 @@
 import { IncomingMessage } from "http";
 import { client } from "./generated-js-runtime-bindings/wasi_http_tests_component.js";
+import type { WasiHttpTestsClient } from "./generated-js-runtime-bindings/interfaces/wasi-http-tests-component-client.js";
 import { describe, test, expect } from "vitest";
-import * as workerpool from "workerpool";
 
 describe("Component runtime functions", async () => {
   test("client test", async () => {
     const testClient = new client.WasiHttpTestsClient();
-    // const httpReturnRawBodyData = testClient.httpCall();
-    // const httpReturnJson = JSON.parse(httpReturnRawBodyData);
-    // console.log("PARSED JSON: ", httpReturnJson);
+    const testClient2 = new client.WasiHttpTestsClient();
+    const proxy = createProxy(testClient);
+    const proxy2 = createProxy(testClient2);
 
-    const pool = workerpool.pool();
-    const proxy = new Proxy(testClient, {
-      get(target, prop, receiver) {
-        // console.log("AARGUMENTS:", arguments);
-        const value = target[prop];
-        // console.log("PROP:", prop);
-        // console.log("VAL:", value);
-        if (value instanceof Function && prop === "httpCall") {
-          function wrapper() {
-            //If you need the arguments they are available in this function
-            console.log("ARGUMENTS:", arguments);
-            return new Promise((resolve, reject) => {
-              //Do the work in here to instantace a new WASM module
-              //call the async function, and return/fail based on the return of that
-              try {
-                const blah = target[prop](...arguments);
-                resolve(blah);
-              } catch (err) {
-                reject(err);
-              }
-            });
-          }
-          return wrapper;
-        }
-        return value;
-      },
-    });
-
-    // console.log("PROXY:", proxy);
-
-    // console.log("PROXY FUN:", proxy.httpCall);
-
-    // console.log("PROXY FUN CALLED:", proxy.httpCall());
-
-    // const proxyReturnRaw1 = await proxy.httpCall(5000);
-    // console.log("proxyReturnRaw:", proxyReturnRaw1);
-    // const proxyReturn1 = JSON.parse(proxyReturnRaw1);
-
-    // console.log("proxyReturn:", proxyReturn);
-
-    // expect(proxyReturn1.code).toBe(200);
-    // expect(proxyReturn1.description).toBe("OK");
-
+    //Note that even though proxy and proxy2 are different instances of the
+    //WasiHttpTestsClient class they still access the same compiled wasm module
+    //so these calls still stack
+    const start = Date.now();
     const [proxyReturnRaw1, proxyReturnRaw2] = await Promise.all([
       proxy.httpCall(5000),
-      proxy.httpCall(1000),
+      proxy2.httpCall(5000),
     ]);
+    const elapsed = Date.now() - start;
+    console.log(
+      `seconds elapsed during http call = ${Math.floor(elapsed / 1000)}`
+    );
 
     console.log("proxyReturnRaw1:", proxyReturnRaw1);
     console.log("proxyReturnRaw2:", proxyReturnRaw2);
@@ -69,3 +34,27 @@ describe("Component runtime functions", async () => {
     expect(proxyReturn2.description).toBe("OK");
   });
 });
+
+const createProxy = <T extends Object>(client: T) => {
+  return new Proxy(client, {
+    get(target, prop, receiver) {
+      const value = target[prop];
+
+      if (value instanceof Function && prop === "httpCall") {
+        function wrapper() {
+          //If you need the arguments they are available in this function
+          return new Promise((resolve, reject) => {
+            try {
+              const blah = target[prop](...arguments);
+              resolve(blah);
+            } catch (err) {
+              reject(err);
+            }
+          });
+        }
+        return wrapper;
+      }
+      return value;
+    },
+  });
+};
